513aa2530d9c8078cb63bf87ae1b46eb1d0ec0ca
emran-niftyitsolution <emran@niftyitsolution.com>
2025-08-07 20:46:26 +0600

    Update dependencies, add redirect to login, and enhance global styles
    - Added new dependencies: @hookform/resolvers, @radix-ui/react-label, @radix-ui/react-slot, class-variance-authority, clsx, lucide-react, next-themes, react-hook-form, sonner, tailwind-merge, and zod.
    - Implemented a redirect from the root path to the login page in next.config.ts.
    - Enhanced global styles in globals.css with new CSS variables and imported tw-animate-css.
    - Cleaned up the Home component in page.tsx to return an empty fragment.

---
+++ app/(auth)/login/_components/LoginForm.tsx
@@ -0,0 +1,102 @@
+"use client";
+
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+
+import { Button } from "@/components/ui/button";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import Link from "next/link";
+
+const FormSchema = z.object({
+  username: z.string().min(2, {
+    message: "Username must be at least 2 characters.",
+  }),
+  password: z.string().min(8, {
+    message: "Password must be at least 8 characters.",
+  }),
+});
+
+export default function LoginForm() {
+  const form = useForm<z.infer<typeof FormSchema>>({
+    resolver: zodResolver(FormSchema),
+    defaultValues: {
+      username: "",
+      password: "",
+    },
+  });
+
+  function onSubmit(data: z.infer<typeof FormSchema>) {
+    console.log(data);
+  }
+
+  return (
+    <Card className="w-[400px]">
+      <CardHeader className="text-center">
+        <CardTitle>Login</CardTitle>
+        <CardDescription>Please login in your account</CardDescription>
+      </CardHeader>
+      <CardContent>
+        <Form {...form}>
+          <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-6">
+            <FormField
+              control={form.control}
+              name="username"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Username</FormLabel>
+                  <FormControl>
+                    <Input placeholder="Enter your username" {...field} />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+
+            <FormField
+              control={form.control}
+              name="password"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Password</FormLabel>
+                  <FormControl>
+                    <Input placeholder="Enter your password" {...field} />
+                  </FormControl>
+                  <FormDescription className="text-xs">
+                    Password must a strong password.
+                  </FormDescription>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <Button type="submit" className="w-full">
+              Login
+            </Button>
+            <p className="text-sm text-muted-foreground">
+              Don&apos;t have an account?{" "}
+              <Link href="/register" className="text-primary">
+                Register
+              </Link>
+            </p>
+          </form>
+        </Form>
+      </CardContent>
+    </Card>
+  );
+}
---
+++ app/(auth)/login/page.tsx
@@ -0,0 +1,11 @@
+import LoginForm from "./_components/LoginForm";
+
+const LoginPage = () => {
+  return (
+    <div className="flex h-screen w-screen items-center justify-center">
+      <LoginForm />
+    </div>
+  );
+};
+
+export default LoginPage;
